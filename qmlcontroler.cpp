9741a174c9e84eba8ee17a395ae615083e9ecab5
rguezennec <renaud@rolisteam.org>
2017-06-26 10:24:33 +0200

    fix stuff

--- qmlcontroler.cpp
+++ qmlcontroler.cpp
@@ -119,6 +119,7 @@ void QmlControler::initConnection()
 }
 void QmlControler::currentPageHasChanged(int i)
 {
+    qDebug() << "current page change"<< i;
     m_currentScreen = i;
     QImage img = m_window->grabWindow();
 
@@ -128,8 +129,8 @@ void QmlControler::currentPageHasChanged(int i)
     static int count = 0;
 
 
-    //img.save(tr("screens/%1_screen.png").arg(++count,2,10,QChar('0')),"png");
-    //qDebug() << "screen shot save" << ++count;
+    img.save(tr("screens/%1_screen.png").arg(++count,2,10,QChar('0')),"png");
+    //qDebug() << "screen shot save" << +s+count;
 
     /*m_ratioImage = (double)img.size().width()/img.size().height();
     m_ratioImageBis = (double)img.size().height()/img.size().width();
